75bcdd9999c9c088e2869458bf20a66d7c4904e0
Eduard <etrayt@allmax.team>
2019-04-29 17:50:35 +0700

    Small changes

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div>
-    <p>Ready!</p>
-  </div>
-);
-
-export default App;
---
+++ src/containers/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
+import storage from 'redux-persist/lib/storage';
+import reducer from '../redux/user/reducer';
+// import store from '../redux/store';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+const store = createStore(persistedReducer);
+const persistor = persistStore(store);
+
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <p>Ready!</p>
+    </PersistGate>
+  </Provider>
+);
+
+export default App;
