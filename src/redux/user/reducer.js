e2f9b798e5a241d7b6996f1fd38d83e8eac816b8
Eduard <etrayt@allmax.team>
2019-04-30 14:27:26 +0700

    Reduxsauce added

--- src/redux/user/reducer.js
+++ src/redux/user/reducer.js
@@ -1,28 +1,14 @@
-// import { createReducer } from 'reduxsauce';
+import { createReducer } from 'reduxsauce';
 import Types from './constants';
 
 export const INITIAL_STATE = { time: new Date().toLocaleString() };
 
-// export const changeTimer = (state = INITIAL_STATE, action) => {
-//   return { ...state, time: new Date() };
-// };
-
-// export const HANDLERS = {
-//   [Types.CHANGE_TIME]: changeTimer,
-// };
-
-// export default createReducer(INITIAL_STATE, HANDLERS);
-
-const reducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case Types.CHANGE_TIME:
-      return {
-        ...state,
-        time: new Date().toLocaleString(),
-      };
-    default:
-      return state;
-  }
+export const changeTimer = (state = INITIAL_STATE, action) => {
+  return { ...state, time: new Date().toLocaleString() };
 };
 
-export default reducer;
+export const HANDLERS = {
+  [Types.CHANGE_TIME]: changeTimer,
+};
+
+export default createReducer(INITIAL_STATE, HANDLERS);
